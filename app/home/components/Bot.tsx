3df7647361e70db7785257c602dd3a5bbf0d873d
Fisayoafolabi <82701075+Fisayoafolabi@users.noreply.github.com>
2024-04-25 19:59:09 +0100

    Bot

--- app/home/components/Bot.tsx
+++ app/home/components/Bot.tsx
@@ -13,22 +13,21 @@ export default function Bot() {
   };
 
   return (
-    <div className='m-10 '>
+    <div className='mt-16 mr-10 '>
       {isChatbotVisible && (
         <div className='chatBot bg-white shadow-lg rounded-lg max-w-md border-t-2'>
           <div className='border-b-2 border-slate-300 px-2 py-4 flex items-center justify-between'>
-            <div className='inline-flex item-center'>
-              <SiBilibili className='w-8 text-black' />
-              <span className='ml-8'>Bot</span>
+            <div className=' inline-flex item-center'>
+              <SiBilibili className='w-8 h-8 text-black' />
+              <span className=' text-black pt-1 pl-4'>Chatbot</span>
             </div>
-            <MdClose className='text-white w-7 h-7' onClick={toggleChatbot} />
           </div>
-          <div className='h-80 flex flex-col space-y-4 max-w-md px-2 mb-2 mt-2'>
+          <div className='bot h-80 sm:h-60 flex flex-col space-y-4 max-w-md px-2 mb-2 mt-2'>
             <div className='flex flex-col items-start'>
-              <span className='bg-blue-500 px-2 py-4 rounded-b-xl rounded-tr-xl mb-2 mt-2'>Hi can I help you</span>
+              <span className='bg-blue-500 px-2 py-4 rounded-b-xl rounded-tr-xl mb-2 mt-2 text-white'>Hi can I help you</span>
             </div>
-            <div className='flex flex-col items-end'>
-              <span className='bg-gray-400 px-2 py-4 mt-2 mb-2 rounded-b-xl rounded-tl-xl'>Good place for a coffee</span>
+            <div className='user flex flex-col items-end'>
+              <span className='bg-gray-400 px-2 py-4 mt-2 mb-2 rounded-b-xl rounded-tl-xl text-white'>Good place for a coffee</span>
             </div>
           </div>
           <div className=''>
@@ -44,12 +43,12 @@ export default function Bot() {
           </div>
         </div>
       )}
-      <div className='fixed right-4 bottom-11'>
+      <div className='fixed right-4 bottom-8 sm:bottom-11 md:bottom-[470px] lg:bottom-10'>
         <Button
           className="chatbot-toggler h-16 w-16 rounded-full bg-purple-600 text-white flex items-center justify-center focus:outline-none hover:bg-purple-700 transition duration-200 ease-in-out"
           onClick={toggleChatbot}
         >
-            {isChatbotVisible ? <MdClose className='text-white w-7 h-7' /> : <SiChatbot className='text-white w-7 h-7' />}
+            {isChatbotVisible ? <MdClose className='text-white w-5 h-5' /> : <SiChatbot className='text-white w-5 h-5' />}
         </Button>
       </div>
     </div>
